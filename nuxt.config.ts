dcfbdb6b73ff8782b86fa51df62c780cb0dedfc2
luca.costa <100123915+luca-costa@users.noreply.github.com>
2024-06-14 12:27:33 +0200

    init

---
+++ nuxt.config.ts
@@ -0,0 +1,20 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: ["@nuxt/scripts"],
+  scripts: {
+    registry: {
+      googleAnalytics: true,
+    }
+  },
+  // you need to provide a runtime config to access the environment variables
+  runtimeConfig: {
+    public: {
+      scripts: {
+        googleAnalytics: {
+          id: process.env.NUXT_PUBLIC_SCRIPTS_GOOGLE_ANALYTICS_ID
+        },
+      },
+    },
+  },
+})
