c2b98b10110d2c18135199ae72fa8da508b2b6b2
luca.costa <100123915+luca-costa@users.noreply.github.com>
2024-06-17 12:37:39 +0200

    test: gtag function on hp

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,11 @@
 const { gtag, $script } = useScriptGoogleAnalytics({
   id: useRuntimeConfig().public.scripts.googleAnalytics.id
 })
+gtag('event', 'page_view', {
+  page_title: 'Home',
+  page_location: 'https://example.com',
+  page_path: '/',
+})
 </script>
 <template>
     <div>
